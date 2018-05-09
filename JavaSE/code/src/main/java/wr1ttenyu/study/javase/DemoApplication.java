c1ad6cd0eb55b38383ba485fb315e181edc0faa6
e <xingyu.zhao@bangdao-tech.com>
2018-05-09 16:19:09 +0800

    javase study init

---
+++ JavaSE/code/src/main/java/wr1ttenyu/study/javase/DemoApplication.java
@@ -0,0 +1,12 @@
+package wr1ttenyu.study.javase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
---
+++ JavaSE/code/src/test/java/wr1ttenyu/study/javase/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package wr1ttenyu.study.javase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
